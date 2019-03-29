2f8c2443f85e19e30b1db05bc77d0732a0d42c4d
Victoria Fluharty <victoria.fluharty@gmail.com>
2019-03-29 21:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Code your solution here!
 
 function printString(myString) {
-  console.log(myString[0])
+  console.log(myString[0]);
+  
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length)
+    printString(mySubString);
+  } else {
+    return true;
+  }
 }
